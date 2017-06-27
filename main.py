45ee62a81d4b2801e607571a694921de97ecfa09
Taylor Haase <taylor_haase@hotmail.com>
2017-06-27 10:34:50 -0700

    Form layouts, routing

--- main.py
+++ main.py
@@ -12,15 +12,17 @@ class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
 
     def __init__(self, name):
         self.name = name
-        self.completed = False
+
+@app.route('/')
+def index():
+    return redirect('/blog')
 
 
 @app.route('/blog', methods=['POST', 'GET'])
-def index():
+def blog():
 
     if request.method == 'POST':
         blog_name = request.form['blog']
@@ -28,22 +30,23 @@ def index():
         db.session.add(new_blog)
         db.session.commit()
 
-    blogs = Blog.query.filter_by(posted=False).all()
-    posted_blogs = Blog.query.filter_by(completed=True).all()
+    blogs = Blog.query.all()
     return render_template('blog.html',title="Build a Blog", 
-        blogs=blogs, posted_blogs=posted_blogs)
+        blogs=blogs)
 
 
-@app.route('/newpost', methods=['POST'])
-def add_post():
+@app.route('/newpost', methods=['POST','GET'])
+def newpost():
 
-    post_id = int(request.form['post-id'])
-    blog = Blog.query.get(post_id)
-    blog.posted_blogs = True
-    db.session.add(blog)
-    db.session.commit()
+    if request.method == 'POST':
 
-    return redirect('/blog')
+        blog_id = int(request.form['newpost'])
+        newpost = Blog.query.get(blog_id)
+        db.session.add(newpost)
+        db.session.commit()
+
+    return render_template('newpost_form.html', title="Add a Blog Entry")
+
 
 
 if __name__ == '__main__':
