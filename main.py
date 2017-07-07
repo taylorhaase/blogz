732add21d810666741c9ace6e89b944284841c09
Taylor Haase <taylor_haase@hotmail.com>
2017-07-06 22:06:19 -0700

    got db to work

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,8 +14,7 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(1000))
 
-    def __init__(self, name, title, body):
-        self.name = name
+    def __init__(self, title, body):
         self.title = title
         self.body = body
 
@@ -26,50 +25,31 @@ def index():
 
 @app.route('/blog', methods=['POST', 'GET'])
 def blogs():
-    return render_template('blog.html')
-
+    
     blogs = Blog.query.all()
+    return render_template('blog.html',blogs=blogs)
     
 
-@app.route('/newpost', methods=['POST', 'GET'])
+@app.route('/newpost', methods=['POST', 'GET'])    
 def newpost():
-    return render_template('newpost_form.html')
 
-    if request.method == 'POST':
+    if request.method == 'POST':  
+          
         blog_title = request.form['title']
         blog_body = request.form['body']
+
         new_blog = Blog(blog_title, blog_body)
         db.session.add(new_blog)
-
         db.session.commit()
 
-        return render_template('blog.html', title="Build a Blog", 
-        blogs=blogs)
+        blog_id = new_blog.id
 
 
-@app.route('/validate-blog', methods =['POST', 'GET'])
-def entry_error():
-    title = request.form['title']
-    newpost = request.form['newpost']
-
-    title_error = ''
-    blog_error = ''
-
-    newpost = str(newpost)
-    if len(newpost) == 0:
-        blog_error = "Please give your blog some content"
-
-
-    title = str(title)
-    if len(title) == 0:
-        title_error = "Please give your blog a title"
-
-    if not blog_error and not title_error:
-        return redirect('/blog')
+        return redirect(url_for('blogs', id=blog_id))
 
     else:
-        return render_template("newpost_form.html", title_error=title_error, blog_error=blog_error, 
-        title=title, newpost=newpost)
+        return render_template('/newpost_form.html')
+
 
 
 if __name__ == '__main__':
