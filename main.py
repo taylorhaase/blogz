40d13296c4bf55ed337f129f9af7e4f4c7845a63
Taylor Haase <taylor_haase@hotmail.com>
2017-07-10 19:14:14 -0700

    if statements, urls

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:hello123@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-#app.secret_key = 'juh7729018xvcgYHW'
 
 
 class Blog(db.Model):
@@ -28,7 +27,8 @@ def index():
 def blogs():
     
     blogs = Blog.query.all()
-    return render_template('blog.html',blogs=blogs)
+    blog_id = request.args.get('id')
+    return render_template('blog.html',blogs=blogs, id=blog_id)
     
 
 @app.route('/newpost', methods=['POST', 'GET'])
@@ -43,38 +43,35 @@ def newpost():
 
         blog_title = str(blog_title)
         blog_body = str(blog_body)
+        print(blog_body)
 
         if len(blog_title) == 0:
             title_error = 'Please give your blog a title'
-            return render_template('newpost_form.html', title_error=title_error)
-        elif len(blog_body) == 0:
+            return render_template('newpost_form.html', title_error=title_error, 
+                blog_body=blog_body)
+
+        if len(blog_body) == 0:
             body_error = 'Please give your blog some content'
-            return render_template('newpost_form.html', body_error=body_error)
+            return render_template('newpost_form.html', body_error=body_error,
+                blog_title=blog_title)
         
-        if not title_error and not body_error:
-        
-            #if request.method == 'POST':
-                    
-                #blog_title = request.form['title']
-                #blog_body = request.form['body']
+        else:
+            new_blog = Blog(blog_title, blog_body)
+            db.session.add(new_blog)
+            db.session.commit()
 
-                new_blog = Blog(blog_title, blog_body)
-                db.session.add(new_blog)
-                db.session.commit()
-
-                blog_id = new_blog.id
-
-                #return redirect(url_for('blogs', id=blog_id))
-                return redirect('/display?id={0}'.format(blog_id))
+            blog_id = new_blog.id
+           #return redirect(url_for('blogs', id=blog_id))
+            return redirect('/display?id={0}'.format(blog_id))
 
     else:
         return render_template('newpost_form.html')
 
-@app.route('/display')
+@app.route('/display', methods = ['POST', 'GET'])
 def display():
-    blog_id = request.args.get('new_blog.id')
-    title = request.args.get('blog_title')
-    body = request.args.get('blog_body')
+    blog_id = request.args.get('id')
+    title = request.args.get('title')
+    body = request.args.get('body')
     return render_template('display_blog.html', blog_id=blog_id, title=title, body=body)
 
 
