eaa7f285ac1ab3aef06621e72a8f46d68504e721
Taylor Haase <taylor_haase@hotmail.com>
2017-07-11 21:48:41 -0700

    created User db

--- main.py
+++ main.py
@@ -3,20 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:hello123@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:hello123@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-
 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(1000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
    
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    password = db.Column(db.String(20))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -37,6 +48,8 @@ def newpost():
 
         blog_title = request.form['title']
         blog_body = request.form['body']
+                #added 'owner'
+        owner = request.form['owner']
 
         title_error = ''
         body_error = ''
@@ -55,7 +68,8 @@ def newpost():
                 blog_title=blog_title)
         
         else:
-            new_blog = Blog(blog_title, blog_body)
+                                #added owner to new_blog
+            new_blog = Blog(blog_title, blog_body, owner)
             db.session.add(new_blog)
             db.session.commit()
 
